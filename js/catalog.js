7a4bc4bc321a3a94a80dbba57f4ec6f930b8e447
Jesus Alatorre <chuyalatorre@gmail.com>
2019-05-09 22:39:03 -0500

    hover

--- js/catalog.js
+++ js/catalog.js
@@ -1,9 +1,9 @@
-$(document).ready(() => {
-  $('.show-full-card').on('click', () => {
+$(document).ready(function () {
+  $('.show-full-card').on('click', function () {
     const petFullCard = $(this).parent().next();
-    const petProfileContainer = petFullCard.parent();
-    petProfileContainer.css({'width': '100%'});
-    petFullCard.css({'display': 'block'});
+    const petProfileContainer = $(this).parent().parent();
+    $(petProfileContainer).css({'width': '100%'});
+    $(petFullCard).css({'display': 'block'});
   })
 
   $('.close-full-card').on('click', () => {
